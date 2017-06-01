cb49525d398426aeaf2746df0b88ff31dc427aa9
Siva Ninala <Siva_Ninala@epam.com>
2017-06-01 11:37:46 +0530

    removing individual applications and adding a single application for cat clicker and cat voter

--- CatApplication/assets/js/controllers/catsController.js
+++ CatApplication/assets/js/controllers/catsController.js
@@ -1,36 +1,49 @@
 (function () {
-    var CatsController = function () {
-        var vm = this;
+    var CatsController = function ($filter) {
+        var vm = this, cats;
         vm.selectedCat = null;
-        vm.cats = [{
+        vm.selectedOrder = 'name';
+        cats = [{
             name: 'First Cat',
             imgSrc: './assets/images/first_cat.jpg',
-            clicks: 0
+            clicks: 0,
+            isVisited: false
         }, {
             name: 'Second Cat',
             imgSrc: './assets/images/second_cat.jpg',
-            clicks: 0
+            clicks: 0,
+            isVisited: false
         }, {
             name: 'Third Cat',
             imgSrc: './assets/images/third_cat.jpg',
-            clicks: 0
+            clicks: 0,
+            isVisited: false
         }, {
             name: 'Fourth Cat',
             imgSrc: './assets/images/fourth_cat.jpg',
-            clicks: 0
+            clicks: 0,
+            isVisited: false
         }, {
             name: 'Fifth Cat',
             imgSrc: './assets/images/fifth_cat.jpg',
-            clicks: 0
+            clicks: 0,
+            isVisited: false
         }]
+        
+        vm.cats = cats;
 
         vm.incrementClickCount = function (cat) {
             cat.clicks++;
         }
         vm.selectCat = function (cat) {
             vm.selectedCat = cat;
+            vm.selectedCat.isVisited = true;
         }
+        vm.searchCats = function () {
+            var model = vm.searchText;
+            vm.cats = $filter('filter')(cats, { name: model });
+        };
     }
-    angular.module('catClicker').controller('CatsController', CatsController);
+    angular.module('catsApp').controller('CatsController', CatsController);
 
 }())
